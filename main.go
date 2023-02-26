fbc434283e103c3bd711fed7ed14fb15cc993c0a
yrs147 <yashraj14700728@gmail.com>
2023-02-26 12:12:17 +0530

    Created Employee and MongoInstance structs

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main 
 
-import()
+import(
+	"github.com/gofiber/fiber/v2"
+)
+
+type MongoInstance struct {
+	Client 
+	Db
+}
+
+var mg MongoInstance
+
+const dbName = "fiber-hrms"
+const mongoURL = "mongodb://localhost:27017" + dbName
+
+type Employee struct {
+	ID		string
+	Name	string
+	Salary	float64
+	Age		float64
+}
+
+func Connect() error{
+
+}
+
 
 func main(){
-	
+	app := fiber.New()
 }
