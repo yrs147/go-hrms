7ccf4211c4b10a9eb63a30c22fcb1d8754f32c7a
yrs147 <yashraj14700728@gmail.com>
2023-02-26 12:29:37 +0530

    Created mongo connect function

--- main.go
+++ main.go
@@ -1,31 +1,70 @@
-package main 
+package main
 
-import(
+import (
+	"log"
+	"time"
+	"context"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/bson/primitive"
+
+
+
 	"github.com/gofiber/fiber/v2"
 )
 
 type MongoInstance struct {
-	Client 
-	Db
+	Client *mongo.Client
+	Db	   *mongo.Database
 }
 
 var mg MongoInstance
 
 const dbName = "fiber-hrms"
-const mongoURL = "mongodb://localhost:27017" + dbName
+const mongoURI = "mongodb://localhost:27017" + dbName
 
 type Employee struct {
-	ID		string
-	Name	string
-	Salary	float64
-	Age		float64
+	ID     string
+	Name   string
+	Salary float64
+	Age    float64
 }
 
-func Connect() error{
+func Connect() error {
+	client , err := mongo.NewClient(options.Client.ApplyURL(mongoURI))
+	ctx,cancel := context.WithTimeout(context.Background(),30*time.Second)
+	defer cancel()
+
+	err = client.Connect(ctx)
+	db := client.Database(dbName)
+
+	if err != nil {
+		return err
+	}
+
+	mg = MongoInstance{
+		Client: client,
+		Db: db,
+	}
+	return nil
+
 
 }
 
+func main() {
 
-func main(){
+	if err := Connect(); err != nil {
+		log.Fatal(err)
+	}
+
 	app := fiber.New()
+
+	app.Get("/employee", func(c *fiber.Ctx) error {
+
+	})
+	app.Post("/employee")
+	app.Put("/employee/:id")
+	app.Delete("/employee/:id")
+
 }
